d89e9f3e1b353b910d74cc9cf2117e9e1de5bcc3
muyeen <muyeenulislamsakif@gmail.com>
2023-11-03 17:04:08 +0600

    has common

--- Big O/containsCommonItemOptimized.js
+++ Big O/containsCommonItemOptimized.js
@@ -9,24 +9,17 @@ const arrToMap = (arr1) => {
       map[item1] = true;
     }
   });
-  // for (let i = 0; i < arr1.length; i++) {
-  //   if (!map[arr1[i]]) {
-  //     const item = arr1[i];
-  //     map[item] = true;
-  //   }
-  // }
+
   return map;
 };
 
 const arrInMap = (map, arr2) => {
-  const hasCommmon = arr2.map((item) => (map[item] ? true : false));
-
-  // for (let j = 0; j < arr2.length; j++) {
-  //   if (map[arr2[j]]) {
-  //     return true;
-  //   }
-  // }
-  return hasCommmon;
+  for (let j = 0; j < arr2.length; j++) {
+    if (map[arr2[j]]) {
+      return true;
+    }
+  }
+  return false;
 };
 
 const containsCommonItem2 = (arr1, arr2) => {
@@ -34,5 +27,5 @@ const containsCommonItem2 = (arr1, arr2) => {
   return arrInMap(map, arr2); //O(b)
 };
 
-// Big O(a + b)
+// Big O(a + b) --> Big O(n)
 console.log(containsCommonItem2(array1, array2));
