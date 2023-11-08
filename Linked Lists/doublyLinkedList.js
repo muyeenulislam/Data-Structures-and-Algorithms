83b884564d9eb5b2de19b9209cb3e2df2e07a1c9
muyeen <muyeenulislamsakif@gmail.com>
2023-11-08 22:01:05 +0600

    doubly linked list

---
+++ Linked Lists/doublyLinkedList.js
@@ -0,0 +1,103 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
+    this.length++;
+    //   console.log(this)
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+  // reverse() {
+  //   if (!this.head.next) {
+  //     return this.head;
+  //   }
+  //   let first = this.head;
+  //   this.tail = this.head;
+  //   let second = first.next;
+
+  //   while(second) {
+  //     const temp = second.next;
+  //     second.next = first;
+  //     first = second;
+  //     second = temp;
+  //   }
+
+  //   this.head.next = null;
+  //   this.head = first;
+  //   return this.printList();
+  // }
+}
+
+const myLinkedList = new DoublyLinkedList(10);
+myLinkedList;
+myLinkedList.append(5);
+console.log(myLinkedList.append(16));
+console.log(myLinkedList.prepend(1));
+myLinkedList.insert(2, 99);
+console.log(myLinkedList.insert(20, 88));
+console.log(myLinkedList.printList());
+//   myLinkedList.remove(2)
+//   myLinkedList.reverse()
